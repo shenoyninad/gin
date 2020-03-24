516d14941bac525dc38ad9ba5d135bd59aa39f3f
Ninad Shenoy <ninad.shenoy@cerner.com>
2020-03-25 00:55:33 +0530

    Updates

---
+++ precache-manifest.f24e0c7a4b8cc0c3d6a94059056d6b37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80cc0ffda21f60323740e819403df6a5",
+    "url": "/gin/index.html"
+  },
+  {
+    "revision": "11415fd7a77c5a0b9530",
+    "url": "/gin/static/css/main.246c1fdd.chunk.css"
+  },
+  {
+    "revision": "7b1f49c84a90f5820100",
+    "url": "/gin/static/js/2.adbf11f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gin/static/js/2.adbf11f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11415fd7a77c5a0b9530",
+    "url": "/gin/static/js/main.c950d322.chunk.js"
+  },
+  {
+    "revision": "ab179a0d069aa17edfea",
+    "url": "/gin/static/js/runtime-main.d0fd2a26.js"
+  },
+  {
+    "revision": "fc7a9adb43de8121ae54ab98f3791950",
+    "url": "/gin/static/media/COLLEGIA.fc7a9adb.ttf"
+  }
+]);
